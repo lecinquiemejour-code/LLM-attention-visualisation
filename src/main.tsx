66759485e3b5f5b29efc8ea151800a27f97ec241
L5J <lecinquiemejour@gmail.com>
2026-03-02 18:00:00 +0100

    feat: Add GPL-3.0 license and copyright notices
    Includes the full GNU General Public License v3 and adds copyright headers to source files.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+/**
+ * LLM Attention Visualization
+ * Copyright (C) 2026 jnl.ootsidebox@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
